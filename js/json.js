dce0631a849b3116eb8b3a227f9b02371be80df6
unknown <>
2016-09-20 17:09:39 +0800

    2016、9、20

---
+++ js/json.js
@@ -0,0 +1,44 @@
+{"user":[
+{"userID":"00000000","username":"用户名1","Registration time":"2013.07.16","telephone":"13546576849"
+},
+{
+"userID":"11111111","username":"用户名2","Registration time":"2013.07.16","telephone":"13546576849"	
+},
+{
+"userID":"22222222","username":"用户名3","Registration time":"2013.07.16","telephone":"13546576849"
+},
+{
+"userID":"33333333","username":"用户名4","Registration time":"2013.07.16","telephone":"13546576849"
+},
+{
+"userID":"44444444","username":"用户名5","Registration time":"2013.07.16","telephone":"13546576849"
+},
+{
+"userID":"55555555","username":"用户名6","Registration time":"2013.07.16","telephone":"13546576849"
+}
+],	
+"diaochang":[
+{
+"diaochangID":"00000","diaochangname":"钓场1","diaochangstyle":"天坑","telephone"	:"刘宗宇——13546576849"
+},
+{
+"diaochangID":"11111","diaochangname":"钓场2","diaochangstyle":"江河","telephone"	:"刘宗宇——13546576849"
+},
+{
+"diaochangID":"22222","diaochangname":"钓场3","diaochangstyle":"斤塘","telephone"	:"刘宗宇——13546576849"
+},
+{
+"diaochangID":"33333","diaochangname":"钓场4","diaochangstyle":"水库","telephone"	:"刘宗宇——13546576849"
+},
+{
+"diaochangID":"44444","diaochangname":"钓场5","diaochangstyle":"农家乐","telephone":"刘宗宇——13546576849"
+},
+{
+"diaochangID":"55555","diaochangname":"钓场6","diaochangstyle":"黑坑","telephone"	:"刘宗宇——13546576849"
+}
+]
+
+
+
+
+}
